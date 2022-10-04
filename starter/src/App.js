6d9cf8a17f813d7a9732c8698ec2960888821968
Asitha Navaratne <asithan476@gmail.com>
2022-10-04 20:09:31 +0530

    Added reducers to cartSlice and created Modal

--- starter/src/App.js
+++ starter/src/App.js
@@ -1,9 +1,21 @@
 import Navbar from "./components/Navbar";
 import CartContainer from "./components/CartContainer";
+import Modal from "./components/Modal";
+import { useSelector, useDispatch } from "react-redux";
+import { calculateTotals } from "./features/cart/cartSlice";
+import { useEffect } from "react";
 
 function App() {
+  const { cartItems } = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(calculateTotals());
+  }, [cartItems]);
+
   return (
     <main>
+      <Modal />
       <Navbar />
       <CartContainer />
     </main>
--- starter/src/components/CartContainer.jsx
+++ starter/src/components/CartContainer.jsx
@@ -1,8 +1,10 @@
 import CartItem from "./CartItem";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { clearCart } from "../features/cart/cartSlice";
 
 function CartContainer() {
   const { cartItems, total, amount } = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
 
   if (amount < 1) {
     return (
@@ -28,10 +30,12 @@ function CartContainer() {
         <hr />
         <div className="cart-total">
           <h4>
-            total <span>${total}</span>
+            total <span>${total.toFixed(2)}</span>
           </h4>
         </div>
-        <button className="btn clear-btn">clear cart</button>
+        <button className="btn clear-btn" onClick={() => dispatch(clearCart())}>
+          clear cart
+        </button>
       </footer>
     </section>
   );
